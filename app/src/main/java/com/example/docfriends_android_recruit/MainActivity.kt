38ba028477d143ac50360ec3dc6739db7c25ac4d
Teddy Park <tg911022@naver.com>
2021-08-19 18:11:36 +0900

    project_build

---
+++ app/src/main/java/com/example/docfriends_android_recruit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.docfriends_android_recruit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
